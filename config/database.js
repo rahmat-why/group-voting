81d62e037ed10915d705d05d78c1ff093e0e3b71
root <rahmatwahyubudiyanto@gmail.com>
2022-09-18 19:43:07 +0700

    dynamic config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 import { Sequelize, QueryTypes } from'sequelize'
-const database = new Sequelize('bot_wa_grouping', 'root', '', {
+const database = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
   
